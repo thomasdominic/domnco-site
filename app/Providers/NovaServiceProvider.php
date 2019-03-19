639e7dc336a8f974752314010f44fa9fb5d85295
thomasdominic <dominic.thomas76@gmail.com>
2019-03-19 11:08:35 +0000

    Apply fixes from StyleCI

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,7 +43,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                'dominic.thomas76@gmail.com'
+                'dominic.thomas76@gmail.com',
             ]);
         });
     }
