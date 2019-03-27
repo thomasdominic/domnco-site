688dc8e54fe7f777f523a58e982e51715299ad96
Dominic THOMAs <dominic.thomas76@gmail.com>
2019-03-27 20:45:58 +0100

    update factory pour retrouver qui est qui

--- database/factories/ExperienceFactory.php
+++ database/factories/ExperienceFactory.php
@@ -4,8 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Experience::class, function (Faker $faker) {
     return [
-        'title' => ['en' => $faker->text(20), 'fr' => $faker->text(20)],
-        'description' => ['en' => $faker->text(200), 'fr' => $faker->text(200)],
+        'title' => ['en' => 'en Exp '.$faker->text(20), 'fr' => 'fr Exp '.$faker->text(20)],
+        'description' => ['en' => 'en Exp '.$faker->text(200), 'fr' => 'fr Exp '.$faker->text(200)],
         'begin_at' => $faker->dateTime('now'),
         'finish_at' => $faker->dateTime('now'),
         'customer_id' => factory(\App\Customer::class)->create()->id,
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,8 +6,8 @@ $factory->define(App\Post::class, function (Faker $faker) {
     return [
 //        'title' => $faker->text(50),
 //        'text' => $faker->text(500),
-        'title' => ['en' => $faker->text(50), 'fr' => $faker->text(50)],
-        'text' => ['en' => $faker->text(500), 'fr' => $faker->text(500)],
+        'title' => ['en' => 'en Post '.$faker->text(50), 'fr' => 'fr Post '.$faker->text(50)],
+        'text' => ['en' => 'en Post '.$faker->text(500), 'fr' => 'fr Post '.$faker->text(500)],
         'published_at' => $faker->dateTime('now'),
         'user_id' => 1,
         'created_at' => $faker->dateTime('now'),
