f1fbf4d21370c66160ab906616073be10151bccc
thomasdominic <dominic.thomas76@gmail.com>
2020-04-30 01:58:12 +0000

    Apply fixes from StyleCI

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,8 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
     return [
-//        'title' => $faker->text(50),
-//        'text' => $faker->text(500),
+        //        'title' => $faker->text(50),
+        //        'text' => $faker->text(500),
         'title' => ['en' => 'en Post '.$faker->text(50), 'fr' => 'fr Post '.$faker->text(50)],
         'summary' => ['en' => 'en Post '.$faker->text(120), 'fr' => 'fr Post '.$faker->text(120)],
         'text' => ['en' => 'en Post '.$faker->text(500), 'fr' => 'fr Post '.$faker->text(500)],
